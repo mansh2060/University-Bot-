2fc0631ad3ff8df497efd67c1b62bc946230ad98
Manish <heroicmanish017@gmail.com>
2025-01-14 14:38:49 +0530

    Updated app.js

--- static/app.js
+++ static/app.js
@@ -46,7 +46,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-        fetch('http://127.0.0.1:5000/predict', {
+        fetch('https://university-bot-8sh1.onrender.com/predict', {  // Updated URL
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
@@ -56,7 +56,7 @@ class Chatbox {
           })
           .then(r => r.json())
           .then(r => {
-            let msg2 = { name: "Jarvis", message: r.answer };
+            let msg2 = { name: "Assistant", message: r.answer };
             this.messages.push(msg2);
             this.updateChatText(chatbox)
             textField.value = ''
@@ -71,7 +71,7 @@ class Chatbox {
     updateChatText(chatbox) {
         var html = '';
         this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Jarvis")
+            if (item.name === "Assistant")
             {
                 html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
             }
